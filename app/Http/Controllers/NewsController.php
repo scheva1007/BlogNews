9ab35fa3fa0d20c198a74e7de2a8c6c7fcedfb6b
SlavikShev <12061998sl>
2024-02-16 22:06:46 +0200

    Added Request for News

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,94 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreNewsRequest;
+use App\Http\Requests\UpdateNewsRequest;
 use App\Models\Category;
 use App\Models\News;
 use App\Models\Rating;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class NewsController extends Controller
 {
     public function index()
     {
-        $news=News::latest()->paginate(5);
-        $categories=Category::all();
-        $limitMonths=Carbon::now()->subMonths(3);
-        $topNews=News::select()->whereDate('created_at', ">=", $limitMonths)->orderByDesc('rating')->limit(3)->get();
+        $news = News::latest()->paginate(5);
+        $categories = Category::all();
+        $limitMonths = Carbon::now()->subMonths(3);
+        $topNews = News::select()->whereDate('created_at', ">=", $limitMonths)->orderByDesc('rating')->limit(3)->get();
 
-
-        return  view('news.index', compact('categories', 'news', 'topNews'));
+        return view('news.index', compact('categories', 'news', 'topNews'));
     }
 
     public function create()
     {
-        $user=auth()->user();
-        if(!$user || !$user->isAdmin() && !$user->isAuthor()){
+        $user = auth()->user();
+        if (!$user || !$user->isAdmin() && !$user->isAuthor()) {
             abort(403, 'Unauthorized action.');
         }
-        $categories=Category::all();
+        $categories = Category::all();
 
-        return view ('news.create', compact('categories'));
-
+        return view('news.create', compact('categories'));
     }
 
-    public function store(Request $request)
+    public function store(StoreNewsRequest $request)
     {
-         $request->validate([
-             'title' =>' required',
-             'text' => 'required',
-             'category_id' => 'required|exists:categories,id',
-             'photo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-
-         ]);
-
-         $news=News::create([
-             'title' => $request->title,
-             'content' => $request->text,
-             'category_id' => $request->category_id,
-             'user_id' => auth()->id(),
-         ]);
+        $news = News::create([
+            'title' => $request->title,
+            'content' => $request->text,
+            'category_id' => $request->category_id,
+            'user_id' => auth()->id(),
+        ]);
 
         if ($request->hasFile('photo')) {
             $photoPath = $request->file('photo')->store('news_photos', 'public');
             $news->update(['photo' => $photoPath]);
         }
 
-         return redirect()->route('news.index');
+        return redirect()->route('news.index');
     }
 
     public function show($id)
     {
-        $userId=auth()->id();
-        $news=News::findOrFail($id);
-        $comments=$news->comment;
-        $rating=$news->rating;
-        $userRating=$news->userRating();
+        $userId = auth()->id();
+        $news = News::findOrFail($id);
+        $comments = $news->comment;
+        $rating = $news->rating;
+        $userRating = $news->userRating();
 
-        return view('news.show', compact('news','comments', 'rating', 'userRating', 'userId'));
+        return view('news.show', compact('news', 'comments', 'rating', 'userRating', 'userId'));
     }
 
-    public function edit (News $news)
+    public function edit(News $news)
     {
-        $categories=Category::all();
+        $categories = Category::all();
         return view('news.edit', compact('news', 'categories'));
     }
 
-    public function update (Request $request, News $news)
+    public function update(UpdateNewsRequest $request, News $news)
     {
-
-        $user=auth()->user();
-        if(!$user || !$user->isAdmin() && !$user->isAuthor()){
+        $user = auth()->user();
+        if (!$user || !$user->isAdmin() && !$user->isAuthor()) {
             abort(403, 'Unauthorized action.');
         }
-        $request->validate([
-            'title' => 'required',
-            'text' => 'required',
-            'category_id' => 'required|exists:categories,id',
-            'photo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
 
         $news->update([
             'title' => $request->title,
@@ -98,49 +81,43 @@ class NewsController extends Controller
             'photo' => $request->hasFile('photo') ? $request->file('photo')->store('news_photos', 'public') : $news->photo,
         ]);
 
-
         return redirect()->route('news.index');
     }
 
     public function destroy(News $news)
     {
-        $user=auth()->user();
-        if(!$user || !$user->isAdmin() && !$user->isAuthor()){
+        $user = auth()->user();
+        if (!$user || !$user->isAdmin() && !$user->isAuthor()) {
             abort(403, 'Unauthorized action.');
         }
         $news->delete();
         return redirect()->route('news.index');
     }
 
-    public function rating(Request $request, $id) {
+    public function rating(Request $request, $id)
+    {
         $request->validate([
             'grade' => 'required|integer|min:1|max:5',
         ]);
 
-        $userId=auth()->id();
+        $userId = auth()->id();
 
-        $existingRating=Rating::where('news_id', $id)
+        $existingRating = Rating::where('news_id', $id)
             ->where('user_id', $userId)->first();
 
         if ($existingRating) {
             $existingRating->update(['grade' => $request->input('grade')]);
-
         } else {
-
             Rating::create([
                 'news_id' => $id,
                 'user_id' => $userId,
                 'grade' => $request->input('grade'),
             ]);
         }
-        $newRating=Rating::where('news_id', $id)->avg('grade');
+        $newRating = Rating::where('news_id', $id)->avg('grade');
 
         News::where('id', $id)->update(['rating' => $newRating]);
 
-
-
         return redirect()->back()->with('success', "Вы успешно оценили новость");
     }
-
-
 }
