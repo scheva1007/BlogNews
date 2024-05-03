fc3ca1979b94e2d1dea01505a071de69a822699d
scheva1007 <olegsh1007@gmail.com>
2024-05-03 15:42:32 +0300

    Adding api endpoints for news

--- app/Http/Controllers/Api/NewsApiController.php
+++ app/Http/Controllers/Api/NewsApiController.php
@@ -3,18 +3,58 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Request\StoreNewsApiRequest;
+use App\Http\Request\UpdateNeawsApiRequest;
 use App\Http\Resources\NewsIndexResource;
+use App\Models\News;
 use App\Services\NewsService;
 use Illuminate\Http\Request;
+use OpenApi\Annotations as OA;
 
 class NewsApiController extends Controller
 {
-    public function index (Request $request, NewsService $newsService)
+    public function index(Request $request, NewsService $newsService)
     {
         $sortBy = $request->query('sort_by', 'created_at');
         $sortDirection = $request->query('sort_direction', 'desc');
         $news = $newsService->getSortedNews($sortBy, $sortDirection);
 
         return NewsIndexResource::collection($news);
+    }
+
+    public function store(StoreNewsApiRequest $request)
+    {
+        $news = News::create([
+            'title' => $request->title,
+            'content' => $request->text,
+            'category_id' => $request->category_id,
+            'user_id' => auth()->id(),
+        ]);
+
+        return response()->json($news, 201);
+    }
+
+    public function show(News $news)
+    {
+
+        return response()->json($news);
+    }
+
+    public function update (UpdateNeawsApiRequest $request, News $news)
+    {
+        $news->update([
+            'title' => $request->title,
+            'content' => $request->text,
+            'category_id' => $request->category_id,
+        ]);
+
+        return response()->json($news, 200);
+    }
+
+    public function destroy (News $news)
+    {
+        $news->delete();
+
+        return response()->json(null, 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,19 @@ use App\Http\Controllers\Api\NewsApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('adminOrNewsAuthor')->group(function () {
+        Route::put('/news/{news}', [NewsApiController::class, 'update'])->name('newsApi.update');
+        Route::delete('/news/{news}', [NewsApiController::class, 'destroy'])->name('newsApi.destroy');
+    });
+
+    Route::middleware('adminOrAuthor')->group(function () {
+        Route::post('/news', [NewsApiController::class, 'store'])->name('newsApi.store');
+    });
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::get('/news', [NewsApiController::class, 'index'])->name('newsApi.index');
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/news', [NewsApiController::class, 'index'])->name('newsApi.index');
+Route::get('/news/{news}', [NewsApiController::class, 'show'])->name('newsApi.show');
