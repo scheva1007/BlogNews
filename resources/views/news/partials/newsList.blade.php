886569ca2f51f9c4633ba616c0838bcd11fb53ce
scheva1007 <olegsh1007@gmail.com>
2024-06-16 14:58:08 +0300

    added a photo and used partials for allnews

--- resources/views/news/partials/newsList.blade.php
+++ resources/views/news/partials/newsList.blade.php
@@ -1,11 +1,11 @@
 <div>
-    <span style="margin-bottom: 0; margin-top: 5px; font-size: 12px; background-color: lightcyan">{{ $item->formattedDate }} |
+    <span style="margin-bottom: 0; margin-top: 5px; font-size: 12px; background-color: lightcyan; padding: 2px 5px">{{ $item->formattedDate }} |
         <i class="fas fa-eye" style="color: gray;"></i> {{ $item->views }} |
         <i class="fas fa-comment" style="color: gray;"></i> {{ $item->commentCount }} |<span style="margin-left: 5px;"> Рейтинг: {{ $item->rating }}</span>
     </span>
-    <div style="margin-bottom: 12px;">
+    <div style="margin-bottom: 10px;">
         <a href="{{ route('news.show', $item) }} " class="mr-5 main-link "
-           style="font-size: 18px; margin-bottom: 30px;">{{ $item->title }}</a>
+           style="font-size: 20px; margin-bottom: 10px;">{{ $item->title }}</a>
     </div>
 </div>
 
